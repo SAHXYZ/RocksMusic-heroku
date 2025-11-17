a8dfd693be413d235e6971df07f32e3f9465e5d6
SAHXYZ <opsahalam97@gmail.com>
2025-11-18 00:17:29 +0530

    Add files via upload

---
+++ RocksMusic/utils/inline/start.py
@@ -0,0 +1,54 @@
+from pyrogram.types import InlineKeyboardButton
+import config
+from RocksMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP,
+            ),
+        ]
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+    InlineKeyboardButton(
+        text=_["S_B_3"],
+        url=f"https://t.me/{app.username}?startgroup=true",
+    )
+],
+[
+    InlineKeyboardButton(
+        text=_["S_B_4"],
+        callback_data="settings_back_helper",
+    ),
+],
+[
+    InlineKeyboardButton(
+        text=_["S_B_5"],
+        url=f"https://t.me/{config.OWNER_USERNAME}",
+    ),
+    InlineKeyboardButton(
+        text=_["S_B_2"],
+        url=config.SUPPORT_GROUP,
+    ),
+],
+[
+    InlineKeyboardButton(
+        text=_["S_B_6"],
+        url=config.SUPPORT_CHANNEL,
+    ),
+],
+
+    ]
+    return buttons
